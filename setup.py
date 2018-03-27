7de39fe99674e4b623fc8fce8a6f84c6a7bd1a53
Vykstorm <victorruizgomezdev@gmail.com>
2018-03-27 17:19:56 +0200

    Añadido archivo setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+
+setup(
+    name = 'pyepuck_gui',
+    version = '1.0.0',
+    description = 'Tiny graphical user interface implemented in python for web browsers to visualize e-puck robot',
+    url = 'https://github.com/Vykstorm/pyepuck_gui',
+    author = 'Vykstorm',
+    author_email = 'victorruizgomezdev@gmail.com',
+    license = 'MIT',
+    zip_safe = False,
+    packages = [''],
+    install_requires = ['Pillow'],
+    include_package_data = True
+)
