9df3e818b85b31124afbfe141a801d6eaf3c8320
Vykstorm <victorruizgomezdev@gmail.com>
2018-03-27 18:44:20 +0200

    Añadida clase auxiliar para leer la información del robot e-puck a través de una conexión TCP de forma asíncrona

--- epuck_reader.py
+++ epuck_reader.py
@@ -1,5 +1,12 @@
 
-from threading import Thread, Lock
+from threading import Thread, Lock, Condition
+from socket import socket, AF_INET, SOCK_STREAM
+import hashlib
+import struct
+import zlib
+import json
+from types import SimpleNamespace as Namespace
+
 
 
 class EPuckReader(Thread):
@@ -9,12 +16,25 @@ class EPuckReader(Thread):
     estableciendo el parámetro enable_streaming a True (de esta forma, se crea un servidor TCP
     que envía información del robot)
     '''
-    def __init__(self, address, port):
+    def __init__(self, address = 'localhost', port = 19998):
+        '''
+        Inicializa esta instancia.
+        :param address: Es la dirección del servidor donde se esta ejecutando el controlador, por defecto
+        es localhost
+        :param port: Es el puerto del controlador, por defecto 19998
+        '''
         super().__init__()
 
-        self.socket = ''
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((address, port))
+
         self._alive = True
         self._alive_lock = Lock()
+
+        self._data = None
+        self._data_lock = Condition()
+
+        self.start()
 
     @property
     def alive(self):
@@ -26,17 +46,63 @@ class EPuckReader(Thread):
         with self._alive_lock:
             self._alive = state
 
-    def _run(self):
-        pass
-
     def run(self):
         try:
             self._run()
-        except:
-            pass
+        except Exception as e:
+            print(e)
         finally:
             self.socket.close()
+
+    def _run(self):
+        while self.alive:
+            self._read_data()
 
     def close(self):
         self.alive = False
         self.join()
+
+    @property
+    def data(self):
+        with self._data_lock:
+            self._data_lock.wait_for(lambda: not self._data is None)
+            return self._data
+
+    def _read_data(self):
+        raw_data = self._read_raw_data()
+        data_len, data = struct.unpack('!i', raw_data[16:20])[0], raw_data[20:]
+        data = zlib.decompress(data[:data_len])
+        data = json.loads(struct.unpack('!{}s'.format(len(data)), data)[0].decode())
+        data = Namespace(**data)
+
+
+        with self._data_lock:
+            self._data = data
+            self._data_lock.notify_all()
+
+    def _read_raw_data(self):
+        chunk_size = 1 << 11
+
+        def read_chunk():
+            chunk = bytearray()
+            while len(chunk) < chunk_size:
+                result = self.socket.recv(chunk_size - len(chunk))
+                if result == bytearray():
+                    raise IOError()
+                chunk += result
+            return chunk
+
+        def verify_data(data):
+            md5sum = struct.unpack('!16s', data[:16])[0]
+            body = data[16:]
+
+            hasher = hashlib.md5()
+            hasher.update(body)
+            return hasher.digest() == md5sum
+
+
+        data = read_chunk()
+        while not verify_data(data):
+            data += read_chunk()
+
+        return data
