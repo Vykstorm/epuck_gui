487917de09626e3b22deccd84dc91ab57ab62689
Vykstorm <victorruizgomezdev@gmail.com>
2018-03-30 13:06:49 +0200

    Ahora la interfaz de usuario funciona aunque el controlador no se esté ejecutando

--- epuck_gui.py
+++ epuck_gui.py
@@ -2,6 +2,7 @@
 
 import eel
 from epuck_reader import EPuckReader
+from threading import Lock
 
 
 # Opciones para la interfaz de usuario
@@ -20,38 +21,38 @@ app_options = {
 
 @eel.expose
 def get_prox_sensors():
-    return epuck.data.prox_sensors
+    return get_epuck_data().prox_sensors
 
 @eel.expose
 def get_floor_sensors():
-    return epuck.data.floor_sensors
+    return get_epuck_data().floor_sensors
 
 @eel.expose
 def get_light_sensor():
-    value = epuck.data.light_sensor
+    value = get_epuck_data().light_sensor
     # TODO
     return None
 
 @eel.expose
 def get_vision_sensor():
-    image = epuck.data.vision_sensor
+    image = get_epuck_data().vision_sensor
     return image
 
 @eel.expose
 def get_vision_sensor_params():
-    return epuck.data.vision_sensor_params
+    return get_epuck_data().vision_sensor_params
 
 @eel.expose
 def get_leds():
-    return epuck.data.leds
+    return get_epuck_data().leds
 
 @eel.expose
 def get_motors():
-    return epuck.data.motors
+    return get_epuck_data().motors
 
 @eel.expose
 def get_performance_info():
-    data = epuck.data
+    data = get_epuck_data()
     return {
         'elapsed_time' : data.elapsed_time,
         'update_time' : data.update_time,
@@ -59,7 +60,24 @@ def get_performance_info():
         'think_time' : data.think_time
     }
 
+
+_epuck_lock = Lock()
+epuck = None
+
+def get_epuck_data():
+    global epuck
+    with _epuck_lock:
+        if epuck is None or not epuck.alive:
+            while True:
+                try:
+                    epuck = EPuckReader(address = 'localhost', port = 19998)
+                    break
+                except ConnectionRefusedError:
+                    pass
+        return epuck.data
+
+
+
 if __name__ == '__main__':
-    epuck = EPuckReader(address='localhost', port=19998)
     eel.init(static_path)
     eel.start('main.html', options = app_options)
